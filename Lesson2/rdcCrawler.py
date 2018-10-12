3cd6c501bd358c76cb1f3c886438a6a2fbb70768
Stephane Tram <stephane.tram@telecom-paristech.fr>
2018-10-12 14:17:48 +0200

    exercice crawler darty - pas fini dans le temps

---
+++ Lesson2/rdcCrawler.py
@@ -0,0 +1,76 @@
+import requests
+from bs4 import BeautifulSoup
+from bs4 import NavigableString
+import sys
+
+def getSoupFromUrl(url,stock_name):
+    res = requests.get(url+stock_name+'.html')
+    html_doc = res.text.replace('&nbsp;',' ').replace("\t", "").replace("\r", "").replace("\n", "")
+    soup = BeautifulSoup(html_doc,'html.parser')
+    return soup
+
+
+def getModulesFromColumns(soup):
+
+    columns=soup.find_all('div',{'class':'prix_barre_liste'})
+    modules = [item for sublist in [x.find_all('p', class_='darty_prix_barre_remise darty_small separator_top') for x in columns] for item in sublist]
+    print(modules)
+    return modules
+
+def getModulesFromHeader(soup):
+    siteHeader = soup.find_all('div', {'id':'sectionHeader'})
+    modules = [item for sublist in [x.find_all('div', class_='module') for x in siteHeader] for item in sublist]
+    return modules
+
+def parseModules(modules):
+    dataTable = {}
+    for i in range(len(modules)):
+        ## Traitement des cadres dans le bandeau recap
+        details = modules[i].find_all('div',{'class':'sectionQuote nasdaqChange'})
+        if len(details)>0:
+            rowData = {}
+            rowData[details[0].find_all('span')[0].string] = details[0].find_all('span')[1].string
+            dataTable['spot price'] = rowData
+           
+        details = modules[i].find_all('div',{'class':'sectionQuote priceChange'})
+        if len(details)>0:
+            rowData = {}
+            rowData[details[0].find_all('span')[0].string.strip()] = details[0].find_all('span')[4].string.strip()
+            dataTable['spot change'] = rowData
+
+        ## Traitement des tableaux dans le corps de la page
+        header = [x.string for x in modules[i].find_all('th',{'class':'data'})]
+        rows = modules[i].find_all('tr')
+        for x in rows:
+            if not isinstance(x.contents[0],NavigableString):
+                if x.contents[0].has_attr('class'):
+                    if x.contents[0].attrs['class'][0] == 'dataTitle':
+                        dataType = x.contents[0].string.strip()
+
+                if x.has_attr('class') == True:
+                    rowData = {}
+                    tX = x.find_all('td')
+                    for y in range(len(x)):
+                        if not (tX[y].has_attr('class')):
+                            dataLineName = tX[y].string
+                        if tX[y].has_attr('class'):
+                            if tX[y].attrs['class'][0] == 'data':
+                                rowData[dataLineName if len(header) == 0 else header[y-1]]=tX[y].string
+                    dataTable[dataLineName]=rowData
+    return dataTable
+
+def launch_requests(stock_name):
+    url='https://www.darty.com/nav/recherche/'
+    soup = getSoupFromUrl(url,stock_name)
+    dataDict = parseModules(getModulesFromColumns(soup))
+    print(dataDict)
+    return dataDict
+
+if len(sys.argv) <2:
+    stocks = ['acer','dell']
+else:
+    stocks=sys.argv[1].split(',')
+
+for x in stocks:
+    dataDict = launch_requests(x)
+
